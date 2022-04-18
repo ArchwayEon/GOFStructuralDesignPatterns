e30cbb02c5e779081e4dba9940925d1bf4532dcf
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2022-04-18 13:50:48 -0400

    Adapter example

--- AdapterPattern/AdapterApp.cs
+++ AdapterPattern/AdapterApp.cs
@@ -1,61 +1,98 @@
 ﻿using System;
 
-namespace AdapterPattern
+namespace AdapterPattern;
+
+/// <summary>
+/// MainApp startup class for Structural
+/// Adapter Design Pattern.
+/// </summary>
+class MainApp
 {
-   /// <summary>
-   /// MainApp startup class for Structural
-   /// Adapter Design Pattern.
-   /// </summary>
-   class MainApp
-   {
-      /// <summary>
-      /// Entry point into console application.
-      /// </summary>
-      static void Main()
-      {
-         // Create adapter and place a request
-         Target target = new Adapter();
-         target.Request();
-         // Wait for user
-         Console.ReadKey();
-      }
-   }
+    /// <summary>
+    /// Entry point into console application.
+    /// </summary>
+    static void Main()
+    {
+        // Create adapter and place a request
+        Target target = new Adapter();
+        target.Request();
+        Console.WriteLine("Press any key...");
+        Console.ReadKey();
+        FitSquarePegIntoARoundHole();
+        Console.WriteLine("Press any key...");
+        Console.ReadKey();
 
-   /// <summary>
-   /// The 'Target' class
-   /// </summary>
-   class Target
-   {
-      public virtual void Request()
-      {
-         Console.WriteLine("Called Target Request()");
-      }
-   }
+    }
 
-   /// <summary>
-   /// The 'Adapter' class
-   /// </summary>
-   class Adapter : Target
-   {
-      private Adaptee _adaptee = new Adaptee();
-      public override void Request()
-      {
-         // Possibly do some other work
-         //  and then call SpecificRequest
-         Console.WriteLine("Do something else");
-         _adaptee.SpecificRequest();
-      }
-   }
+    static void FitSquarePegIntoARoundHole()
+    {
+        var hole = new RoundHole { Radius = 5 };
+        var roundPeg = new RoundPeg { Radius = 5 };
+        if (hole.Fits(roundPeg))
+        {
+            Console.WriteLine("The hole fits the round peg.");
+        }
 
-   /// <summary>
-   /// The 'Adaptee' class
-   /// </summary>
-   class Adaptee
-   {
-      public void SpecificRequest()
-      {
-         Console.WriteLine("Called SpecificRequest()");
-      }
-   }
+        var smallSquarePeg = new SquarePeg { Width = 5 };
+        var largeSquarePeg = new SquarePeg { Width = 10 };
+        // hole.Fits(smallSquarePeg); // Does not compile
 
+        var smallSquarePegAdapter = new SquarePegAdapter(smallSquarePeg);
+        var largeSquarePegAdapter = new SquarePegAdapter(largeSquarePeg);
+        if (hole.Fits(smallSquarePegAdapter))
+        {
+            Console.WriteLine("The hole fits the small square peg.");
+        }
+        else
+        {
+            Console.WriteLine("The hole does not fit the small square peg.");
+        }
+        if (hole.Fits(largeSquarePegAdapter))
+        {
+            Console.WriteLine("The hole fits the large square peg.");
+        }
+        else
+        {
+            Console.WriteLine("The hole does not fit the large square peg.");
+        }
+
+    }
 }
+
+/// <summary>
+/// The 'Target' class
+/// </summary>
+class Target
+{
+    public virtual void Request()
+    {
+        Console.WriteLine("Called Target Request()");
+    }
+}
+
+/// <summary>
+/// The 'Adapter' class
+/// </summary>
+class Adapter : Target
+{
+    private Adaptee _adaptee = new Adaptee();
+    public override void Request()
+    {
+        // Possibly do some other work
+        //  and then call SpecificRequest
+        Console.WriteLine("Do something else");
+        _adaptee.SpecificRequest();
+    }
+}
+
+/// <summary>
+/// The 'Adaptee' class
+/// </summary>
+class Adaptee
+{
+    public void SpecificRequest()
+    {
+        Console.WriteLine("Called SpecificRequest()");
+    }
+}
+
---
+++ AdapterPattern/AdapterExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern;
+
+public class RoundHole
+{
+    public double Radius { get; set; }
+
+    public bool Fits(RoundPeg peg)
+    {
+        return Radius >= peg.Radius;
+    }
+}
+
+public class SquarePeg
+{
+    public double Width { get; set; }
+}
+
+
+public class RoundPeg
+{
+    private double _radius;
+
+    public virtual double Radius
+    {
+        get => _radius;
+        set => _radius = value;
+    }
+}
+
+public class SquarePegAdapter : RoundPeg
+{
+    private SquarePeg _squarePeg;
+
+    public SquarePegAdapter(SquarePeg squarePeg)
+    {
+        _squarePeg = squarePeg;
+    }
+
+    public override double Radius
+    {
+        // Gets the radius of the circle that fits inside the square peg
+        get => _squarePeg.Width * Math.Sqrt(2.0) / 2.0;
+        // Sets the width of the square peg based on the radius of the circle
+        set
+        {
+            _squarePeg.Width = value / Math.Sqrt(2.0);
+        }
+    }
+}
+
+
